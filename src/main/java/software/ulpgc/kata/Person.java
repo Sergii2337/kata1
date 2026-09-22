0b016e14e508848bf032091f2ca0ff1542370c87
Sergii2337 <sergiodelrionavarro@gmail.com>
2026-09-22 09:20:54 +0100

    Person with birthdate

--- src/main/java/software/ulpgc/kata/Person.java
+++ src/main/java/software/ulpgc/kata/Person.java
@@ -1,13 +1,21 @@
 package software.ulpgc.kata;
 
+import java.time.LocalDate;
+
 public class Person {
     private final String name;
+    private final LocalDate birthDate;
 
-    public Person(String name) {
+    public Person(String name, LocalDate birthDate) {
         this.name = name;
+        this.birthDate = birthDate;
     }
 
     public String name() {
         return name;
+    }
+
+    public LocalDate birthDate() {
+        return birthDate;
     }
 }
